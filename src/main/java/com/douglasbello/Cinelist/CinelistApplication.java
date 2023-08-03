21e6cc3133490590b982458cd90b76061ff332b9
douglasbello <douglasbelloalv1@outlook.com>
2023-08-03 00:34:32 -0300

    just testing new machine ssh

--- src/main/java/com/douglasbello/Cinelist/CinelistApplication.java
+++ src/main/java/com/douglasbello/Cinelist/CinelistApplication.java
@@ -3,7 +3,6 @@ package com.douglasbello.Cinelist;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class CinelistApplication {
